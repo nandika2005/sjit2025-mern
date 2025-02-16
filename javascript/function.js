109babc304693685fbecb8972170a76aa000c458
nandika2005 <145485411+nandika2005@users.noreply.github.com>
2025-02-16 19:26:24 +0530

    task

---
+++ javascript/function.js
@@ -0,0 +1,20 @@
+//parameterized function
+function add(a,b) {
+    console.log(a+b)
+} 
+add(12,10)
+//non-parameterized function
+function greet(){
+    console.log("Hello!,This is non-paramterized function")
+}
+greet();
+//non-parameterized return
+function nonparameterReturn(){
+    return "This is non-parameter return type"
+}
+console.log(nonparameterReturn());
+//Parameterized return func
+function multiply(a,b){
+return a*b
+}
+console.log(multiply(10,20))
