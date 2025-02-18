19fe62d0865a62fc061b46f0967db5bcbaac5a7d
nandika2005 <145485411+nandika2005@users.noreply.github.com>
2025-02-18 22:09:15 +0530

    an

---
+++ popfrontend/src/App.jsx
@@ -0,0 +1,14 @@
+import Home from './components/functionalcompo/Home.jsx';
+
+function App() {
+ 
+
+  return (
+      <div>
+         <h1>Welcome</h1>
+         <Home></Home>
+         </div>
+  )
+}
+
+export default App;
