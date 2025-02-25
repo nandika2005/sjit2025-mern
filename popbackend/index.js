6cfb3d56d8d58a7fb06aa38fb3bda7ed769d957f
nandika2005 <145485411+nandika2005@users.noreply.github.com>
2025-02-25 13:49:13 +0530

    an

--- popbackend/index.js
+++ popbackend/index.js
@@ -1,7 +1,36 @@
 const express=require('express')
 const app=express()
+const mdb=require('mongoose')
+const dotenv=require('dotenv')
+dotenv.config();
+const Signup=require("./models/signupSchema")
+app.use(express.json())
 app.get("/login",(req,res)=>{
      res.sendFile("C:\\Users\\nandi\\OneDrive\\Desktop\\merntrain\\Html with styling\\index.html")
 })
 const port=3002
+mdb.
+connect("mongodb+srv://nandikamaheshwar6:3Br1q4Bx0ebpDtdX@popbackend.oltm4.mongodb.net/test")
+.then(()=>{console.log("MongoDB connected successfully")})
+     .catch((err)=>{
+ console.log(err);
+});
+app.post("/signup",(req,res)=>{
+     try{
+          const {firstName,lastName,email,password,phoneNumber}=req.body
+      const newSignup=new Signup({
+          firstName: firstName,
+          lastName: lastName,
+          email:email,
+          password:password,
+          phoneNumber:phoneNumber
+     });
+     newSignup.save()
+     console.log("Signup successful");
+    res.status(201).json({message:"signup successful",isSignUp:true});
+}catch(err){
+     console.log(err);
+     res.status(201).json({message:"Signup unsuccessful",isSignUp:false})
+}
+})
 app.listen(port,()=>console.log('Server is listening to:',port))
