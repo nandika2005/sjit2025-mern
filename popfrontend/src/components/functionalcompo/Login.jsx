7edf158d7ae6978c8c8c00171f006cdf9f01136d
nandika2005 <145485411+nandika2005@users.noreply.github.com>
2025-02-26 12:20:58 +0530

    frontend

--- popfrontend/src/components/functionalcompo/Login.jsx
+++ popfrontend/src/components/functionalcompo/Login.jsx
@@ -1,12 +1,25 @@
 import { Link } from 'react-router-dom';
 import '../css/Login.css'
+import { useState } from 'react';
+import axios from 'axios'
 function Login(){
+   const [email,setEm]=useState("")
+   const[password,setPass]=useState("")
+   const handleLogin=async(event)=>{
+      event.preventDefault();
+      const req=await axios.post("http://localhost:3002/login",{
+         email: email,
+         password:password,
+      });
+      const message=req.data.message;
+      alert(message);
+   };
    return (<div>
-   <form>
+   <form onSubmit={handleLogin}>
    <label htmlFor="EName">EmailId:</label>
-   <input type="email" placeholder="Enter your mail-id" required/>
+   <input type="email"  value={email} onChange={e=>setEm(e.target.value)} placeholder="Enter your mail-id" required/>
    <label htmlFor="PName">Password:</label>
-   <input type="password" placeholder="Enter your password" required/>
+   <input type="password" value={password}onChange={e=>setPass(e.target.value)} placeholder="Enter your password" required/>
    <button>Submit</button>
    <p><Link to="/Signup">Create an account?</Link></p>
    </form> 
